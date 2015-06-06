a43957d295602030cd2352a1710c5b059371cac7
Randy <randypriv@gmail.com>
2015-06-06 22:52:53 +0800

    add fix bubble

--- scripts/base.js
+++ scripts/base.js
@@ -2,18 +2,34 @@
   $(document).ready(function(){
     var make = function(container,options){
       var div = $('<div class="bubble" />').text(options.content);
-      div.css({
-        "font-size": options.fontsize + "em",
-        "position": "absolute",
-        "color": options.color,
-        "top": Math.random() * $(window).height()
-      })
       container.append(div);
-      div.animate({
-        left: $(window).width()
-      },options.duration,'linear',function(){
-        div.remove();
-      })
+      var bumb = Math.random() * 10 < 5;
+      console.log(bumb);
+      if (!bumb) {
+        div.css({
+          "font-size": options.fontsize + "em",
+          "position": "absolute",
+          "color": options.color,
+          "top": Math.random() * $(document).height()
+        })
+        div.animate({
+          left: $(document).width() + 100
+        },options.duration,'linear',function(){
+          div.remove();
+        })
+      } else {
+        div.css({
+          "font-size": options.fontsize + "em",
+          "text-align": "center",
+          "color": options.color,
+          "margin-top": Math.random() * $(document).height()
+        })
+        window.setTimeout(function(){
+          div.remove();
+        },3000);
+      }
+      
+      
     }
     // make($('#container'),{"color":"#f2ad88","fontsize":1.5729205077514052,"content":"来啦","duration":4541,"nickname":"oH_xis15rtTiWz88QL4AwWKrZEFg"});
 
