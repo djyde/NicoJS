484a3984bbd3691578ed1510c3fa7919d38f90f5
Randy <randypriv@gmail.com>
2015-06-07 03:54:54 +0800

    fix

--- js/base.js
+++ js/base.js
@@ -9,7 +9,7 @@
         div.css({
           "font-size": options.fontsize * 1.8 + "em",
           "position": "absolute",
-          "color": options.color,
+          "color": "red",
           "white-space": "nowrap",
           "right": 0,
           "top": Math.random() * $(window).height()
