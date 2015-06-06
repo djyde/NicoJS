12db83738c4bf0b07cc8069ab2761093c6fa9c29
Randy <randypriv@gmail.com>
2015-06-06 19:36:16 +0800

    0.1

--- scripts/base.js
+++ scripts/base.js
@@ -1,5 +1,31 @@
 ;(function(){
   $(document).ready(function(){
-    alert('test')
+    var make = function(container,options){
+      var div = $('<div class="bubble" />').text(options.content);
+      div.css({
+        "font-size": options.fontsize + "em",
+        "position": "relative",
+        "color": options.color,
+        "top": Math.random() * $(window).height()
+      })
+      container.append(div);
+      div.animate({
+        left: $(window).width()
+      },options.duration,'linear',function(){
+      })
+    }
+    // make($('#container'),{"color":"#f2ad88","fontsize":1.5729205077514052,"content":"来啦","duration":4541,"nickname":"oH_xis15rtTiWz88QL4AwWKrZEFg"});
+
+
+    var socket = io('123.57.143.92:3000');
+
+    socket.on('connect',function(){
+      console.log('1');
+    })
+
+    socket.on('message',function(options){
+      console.log(options);
+      make($('#container'),JSON.parse(options));
+    })
   })
 })()
